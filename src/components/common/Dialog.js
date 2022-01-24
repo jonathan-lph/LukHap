69496dddcfdffaab715906000a36702c6a35a6cb
jonathan-lph <jonathan.lph98@gmail.com>
2022-01-24 17:51:20 +0800

    Added Dialog and modified Header

--- src/components/common/Dialog.js
+++ src/components/common/Dialog.js
@@ -1,7 +1,18 @@
-export default function Dialog() {
-  return (
-    <div>
-      
+import styles from '@styles/common/Dialog.module.sass'
+import { createPortal } from 'react-dom'
+import { Close } from '@components/icons'
+
+export default function Dialog({ open, children, handleClose }) {
+
+  if (!open) return null
+  return createPortal(
+    <div className={styles.backdrop} >
+      <div className={styles.inner} onClick={handleClose}>
+        <dialog className={styles.root} open={open} onClick={e => e.stopPropagation()}>
+          <Close className={styles.close} onClick={handleClose}/>
+          {children}
+        </dialog>
+      </div>
     </div>
-  )
+  , document.getElementById('themed-app'))
 }
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,24 +1,52 @@
 import styles from '@styles/common/Header.module.sass'
 import clsx from 'clsx'
 import { Settings, HelpOutline, Statistics, Cafe } from '@components/icons'
+import { Fragment, useState } from 'react'
+import { Dialog } from '@components/common'
 
 export default function Header() {
+
+  const [open, setOpen] = useState({
+    help: false
+  })
+
+  const handleOpen = key => e => {
+    setOpen({...open, [key]: true})
+  }
+
+  const handleClose = key => e => {
+    setOpen({...open, [key]: false})
+  }
+
   return (
-    <header className={styles.root}>
-      <div className={styles.buttons}>
-        <HelpOutline/>
-        <Cafe/>
-      </div>
-      <div className={styles.title}>
-        Cidou 詞道
-        <span className={styles.subtitle}>
-          （遲到）
-        </span>
-      </div>
-      <div className={styles.buttons}>
-        <Statistics/>
-        <Settings/>
-      </div>
-    </header>
+    <Fragment>
+      <header className={styles.root}>
+        <div className={styles.buttons}>
+          <HelpOutline 
+            className={styles.button}
+            onClick={handleOpen('help')}
+          />
+          <Cafe className={styles.button}/>
+        </div>
+        <div className={styles.title}>
+          Cidou 詞道
+          <span className={styles.subtitle}>
+            （遲到）
+          </span>
+        </div>
+        <div className={styles.buttons}>
+          <Statistics className={styles.button}/>
+          <Settings className={styles.button}/>
+        </div>
+      </header>
+
+      {open.help && 
+        <Dialog
+          open={open.help}
+          handleClose={handleClose('help')}
+        />
+      }
+
+    </Fragment>
   )
 } 
