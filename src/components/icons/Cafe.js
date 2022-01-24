651aa83cf60649effd74ef6fa5bc8a4b79c179c4
jonathan-lph <jonathan.lph98@gmail.com>
2022-01-24 17:49:45 +0800

    Added Close icon and allowed props

--- src/components/icons/Cafe.js
+++ src/components/icons/Cafe.js
@@ -1,8 +1,9 @@
 import styles from '@styles/common/Icons.module.sass'
+import clsx from 'clsx'
 
-export default function Cafe(props) {
+export default function Cafe({className, ...props}) {
   return (
-    <svg viewBox="0 0 24 24" className={styles.root} {...props}>
+    <svg viewBox="0 0 24 24" className={clsx(styles.root, className && className)} {...props}>
       <path d="M0 0h24v24H0V0z" fill="none"/>
       <path d="M20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2c1.11 0 2-.9 2-2V5c0-1.11-.89-2-2-2zm0 5h-2V5h2v3zM4 19h16v2H4z"/>
     </svg>
--- src/components/icons/Statistics.js
+++ src/components/icons/Statistics.js
@@ -1,8 +1,9 @@
 import styles from '@styles/common/Icons.module.sass'
+import clsx from 'clsx'
 
-export default function Statistics(props) {
+export default function Statistics({className, ...props}) {
   return (
-    <svg viewBox="0 0 24 24" className={styles.root} {...props}>
+    <svg viewBox="0 0 24 24" className={clsx(styles.root, className && className)} {...props}>
       <rect fill="none" height="24" width="24"/>
       <path d="M7.5,21H2V9h5.5V21z M14.75,3h-5.5v18h5.5V3z M22,11h-5.5v10H22V11z"/>
     </svg>
