1fb6a3bd2f1d8401843992ffb286ef0ffbcff5b4
jonathan-lph <jonathan.lph98@gmail.com>
2022-01-26 17:40:47 +0800

    Completed StatisticsDialog layout

--- src/components/dialog/StatisticsDialog.js
+++ src/components/dialog/StatisticsDialog.js
@@ -1,29 +1,50 @@
 import { Dialog } from "@components/common";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from '@styles/dialog/StatisticsDialog.module.sass'
 import { Fragment } from "react";
+import { Trophy } from "@components/icons";
+
+const data = [{
+  label: '已遊玩',
+  key: 'gamesPlayed' 
+}, {
+  label: '勝率',
+  key: 'winPercentage'
+}, {
+  label: '連勝',
+  key: 'currentStreak'
+}, {
+  label: '最高連勝',
+  key: 'maxStreak'
+}]
+
+const padNum = num => Math.floor(num).toString().padStart(2, 0)
 
 export default function StatisticsDialog(props) {
   const [stat, setStat] = useState(null)
+  const diffSec = useRef()
+  const countdown = useRef()
 
   useEffect(() => {
     const local = localStorage.getItem('statistics')
     if (local) setStat(JSON.parse(local))
   }, [])
 
-  const data = [{
-    label: '已遊玩',
-    value: stat?.gamesPlayed ?? 0
-  }, {
-    label: '勝率',
-    value: stat?.winPercentage ?? 0
-  }, {
-    label: '連勝',
-    value: stat?.currentStreak ?? 0
-  }, {
-    label: '最高連勝',
-    value: stat?.maxStreak ?? 0
-  }]
+  // Set countdown timer
+  useEffect(() => {
+    const today = new Date()
+    const tmr = new Date()
+    tmr.setDate(tmr.getDate() + 1)
+    tmr.setHours(0, 0, 0, 0)
+    diffSec.current = Math.floor(tmr.valueOf() - today.valueOf()) / 1000
+    const timer = setInterval(() => {
+      if (!countdown.current) return;
+      countdown.current.innerText = 
+        `${padNum(diffSec.current / 3600)}:${padNum(diffSec.current / 60 % 60)}:${padNum(diffSec.current % 60)}`
+      diffSec.current--
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [])
 
   const maxDistribution = stat?.guesses 
     ? Math.max(...Object.values({...stat.guesses, fail: 0}))
@@ -35,15 +56,15 @@ export default function StatisticsDialog(props) {
       title="遊戲統計"
     >
       <dl className={styles.stats}>
-        {data.map(({label, value}) =>
+        {data.map(({label, key}) =>
           <div key={label} className={styles.stat}>
-            <dd>{value}</dd>
+            <dd>{stat?.[key] ?? 0}</dd>
             <dt>{label}</dt>
           </div>
         )}
       </dl>
-      {stat?.guesses &&
-        <dl className={styles.distribution}>
+      {stat?.guesses 
+      ? <dl className={styles.distribution}>
           {[...Array(6)].map((_, i) => {
             const numPercentage = Math.floor(stat.guesses[i+1]/maxDistribution * 100).toFixed(2)
             return (
@@ -63,7 +84,22 @@ export default function StatisticsDialog(props) {
             </Fragment>
           )})}
         </dl>
+      : <div className={styles.emptyDistribution}>
+          未有其他數據
+        </div>
       }
+      <div className={styles.footer}>
+        <dl className={styles.countdown}>
+          <time ref={countdown}>00:00</time>
+          <div>距離下個詞語</div>
+        </dl>
+        <div className={styles.shareDiv}>
+          <button className={styles.share}>
+            <Trophy/>
+            <span>分享本日成績</span>
+          </button>
+        </div>
+      </div>
     </Dialog>
   )
 }
