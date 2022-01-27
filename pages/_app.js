39552de260f8af4599e17024abcf053f51b59f0d
jonathan-lph <jonathan.lph98@gmail.com>
2022-01-27 14:55:23 +0800

    Added responsive app height (for mobile)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import '@styles/global.sass'
 import { ThemeProvider } from '@context/ThemeContext'
 
 function MyApp({ Component, pageProps }) {
+  
+  useEffect(() => {
+    // Configure height
+    const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
+    window.addEventListener('resize', appHeight)
+    appHeight()
+    return () => window.removeEventListener('resize', appHeight)
+  }, []);
+
   return (
     <ThemeProvider>
       <Component {...pageProps} />
