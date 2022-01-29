63bb392a24da095401bfcadda8849af47d95bdbc
jonathan-lph <jonathan.lph@hotmail.com>
2022-01-29 16:33:01 +0800

    Demo 1.1.2

--- _next/static/q2kxa-9C3dej0VpWRU2vm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/719-57a75947860d87e0.js","static/css/1fda5137444b7674.css","static/chunks/pages/index-47097e89baa8482a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
