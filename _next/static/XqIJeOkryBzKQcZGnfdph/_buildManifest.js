36660bc9772838edaa29fc970d2e7285a69d6607
jonathan-lph <jonathan.lph@hotmail.com>
2022-01-30 19:58:47 +0800

    Release 1.0.2 - Added report

--- _next/static/XqIJeOkryBzKQcZGnfdph/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/719-57a75947860d87e0.js","static/css/c8fa88d38fbb1ec8.css","static/chunks/pages/index-6510f573fbfd2950.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
